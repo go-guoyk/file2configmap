859eb0cb874d8891c8435aa34d13a0099ea5856d
Guo Y.K <hi@guoyk.net>
2021-05-12 11:13:41 +0800

    rename argument flag, support key customization

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
 # file2configmap
 
-A tool to convert files into Kubernetes ConfigMap resource
+A tool to convert files into Kubernetes ConfigMap resource with `binaryData` field.
 
 ## Install
 
 ```shell
-go get -u go.guoyk.net/file2configmap
+go install -u go.guoyk.net/file2configmap
 ```
 
 ## Usage
 
+### Files in current directory
+
 ```shell
-file2configmap -name myname -namespace mynamespace file1.txt file2.yaml file3.json
+file2configmap -n myname -ns mynamespace file1.txt
+```
+
+### Files in subdirectories
+
+`file2configmap` will use file base name as `ConfigMap` key.
+
+```shell
+file2configmap -n myname -ns mynamespace path/to/file2.txt
+```
+
+### Customize key name instead of using file name
+
+```shell
+file2configmap -n myname -ns mynamespace file3.json:file3.txt
+```
+
+### Multiple files
+
+```shell
+file2configmap -n myname -ns mynamespace file1.txt path/to/file2.txt files3.json:file3.txt
 ```
 
 ## Credits
--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/base64"
+	"errors"
 	"flag"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type ConfigMap struct {
@@ -31,9 +33,14 @@ func main() {
 
 	var optName string
 	var optNamespace string
-	flag.StringVar(&optName, "name", "unknown", "name of the ConfigMap")
-	flag.StringVar(&optNamespace, "namespace", "default", "namespace of the ConfigMap")
+	flag.StringVar(&optName, "n", "", "name of the ConfigMap")
+	flag.StringVar(&optNamespace, "ns", "default", "namespace of the ConfigMap")
 	flag.Parse()
+
+	if optName == "" {
+		err = errors.New("missing argument -n")
+		return
+	}
 
 	configMap := ConfigMap{
 		APIVersion: "v1",
@@ -44,12 +51,26 @@ func main() {
 	configMap.Metadata.Name = optName
 	configMap.Metadata.Namespace = optNamespace
 
-	for _, file := range flag.Args() {
+	for _, arg := range flag.Args() {
+		var key string
+		var file string
+		argSplit := strings.Split(arg, ":")
+		if len(argSplit) == 1 {
+			key = filepath.Base(argSplit[0])
+			file = argSplit[0]
+		} else if len(argSplit) == 2 {
+			key = argSplit[1]
+			file = argSplit[0]
+		}
+		if key == "" || file == "" {
+			err = errors.New("invalid argument '" + arg + "'")
+			return
+		}
 		var buf []byte
 		if buf, err = ioutil.ReadFile(file); err != nil {
 			return
 		}
-		configMap.BinaryData[filepath.Base(file)] = base64.StdEncoding.EncodeToString(buf)
+		configMap.BinaryData[key] = base64.StdEncoding.EncodeToString(buf)
 	}
 
 	enc := yaml.NewEncoder(os.Stdout)
